13b55a837c0df7d3b89cf384c05f61c56184bc7a
d.fizesan <dan@netmatch.ro>
2023-01-03 16:53:08 +0200

    use no-cache when downloading the files to get the lastest version

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,10 +2,10 @@
 #
 #  Run the following command
 #
-#  wget -q -O setup_environment.sh https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/setup_environment.sh && chmod +x setup_environment.sh && bash setup_environment.sh
+#  wget -q --no-cache -O setup_environment.sh https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/setup_environment.sh && chmod +x setup_environment.sh && bash setup_environment.sh
 #
 ##################################
 
-wget -q -O .bashrc https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.bashrc
+wget -q --no-cache -O .bashrc https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.bashrc
 source .bashrc
-wget -q -O .bash_profile https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.bash_profile
+wget -q --no-cache -O .bash_profile https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.bash_profile
