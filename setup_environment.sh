c978f6438c6bded8a5318fc7ca90f0f34c13f75b
d.fizesan <dan@netmatch.ro>
2023-01-03 15:40:57 +0200

    add echo to setup

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,1 +1,2 @@
-#wget https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/setup_environment.sh
+#wget -q -O setup_environment.sh https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/setup_environment.sh && chmod +x setup_environment.sh
+echo "downloaded"
