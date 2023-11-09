a64bf2f21e580b9ddf0ec85162114c5613420c16
d.fizesan <dan@netmatch.ro>
2023-11-09 09:26:27 +0200

    remove the .bashrc to make sure it is refreshed

--- setup_development.sh
+++ setup_development.sh
@@ -1,3 +1,6 @@
 mkdir -p .azure-cacao
+
+rm .azure-cacao/.bashrc
 wget -q --no-cache -O .azure-cacao/.bashrc https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-cacao/.bashrc
+
 wget -q --no-cache -O .azure-cacao/cacao-icon.png https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-cacao/cacao-icon.png
