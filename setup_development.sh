4f5a0758ca83696c62f8925240f773a870cf9fc3
d.fizesan <dan@netmatch.ro>
2023-11-09 10:42:36 +0200

    add development .azure-gzplein settings

--- setup_development.sh
+++ setup_development.sh
@@ -8,3 +8,8 @@ mkdir -p .azure-corendon
 rm .azure-corendon/.bashrc
 wget -q --no-cache -O .azure-corendon/.bashrc https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-corendon/.bashrc
 wget -q --no-cache -O .azure-corendon/corendon-icon.ico https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-corendon/corendon-icon.ico
+
+mkdir -p .azure-gzplein
+rm .azure-gzplein/.bashrc
+wget -q --no-cache -O .azure-gzplein/.bashrc https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-gzplein/.bashrc
+wget -q --no-cache -O .azure-gzplein/gzplein-icon.ico https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-gzplein/gzplein-icon.ico
