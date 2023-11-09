d6bcd4515413780c09b8f78a63ae508c87748fd5
d.fizesan <dan@netmatch.ro>
2023-11-09 09:42:01 +0200

    add development .azure-corendon configuration

--- setup_development.sh
+++ setup_development.sh
@@ -1,6 +1,10 @@
 mkdir -p .azure-cacao
-
 rm .azure-cacao/.bashrc
 wget -q --no-cache -O .azure-cacao/.bashrc https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-cacao/.bashrc
-
 wget -q --no-cache -O .azure-cacao/cacao-icon.png https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-cacao/cacao-icon.png
+
+
+mkdir -p .azure-corendon
+rm .azure-corendon/.bashrc
+wget -q --no-cache -O .azure-corendon/.bashrc https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-corendon/.bashrc
+wget -q --no-cache -O .azure-corendon/corendon-icon.ico https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.azure-corendon/corendon-icon.ico
