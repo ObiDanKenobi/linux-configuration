21c1ab07f6a11d9ceb382a9953f33a89736b9519
d.fizesan <dan@netmatch.ro>
2023-11-10 13:05:25 +0200

    we do not have setup_development anymore

--- setup_environment.sh
+++ setup_environment.sh
@@ -13,11 +13,6 @@ rm .bash_profile
 wget -q --no-cache -O .bash_profile https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.bash_profile
 source .bash_profile
 
-rm setup_development.sh
-wget -q --no-cache -O setup_development.sh https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/setup_development.sh
-chmod +x setup_development.sh
-
-
 #more info about settings: https://dev.to/blikoor/customize-git-bash-shell-498l
 mkdir -p .config
 rm .config/git-prompt.sh
