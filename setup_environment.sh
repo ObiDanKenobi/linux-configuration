d93706c98fbbd3db96bbbab3724748db0f094638
dan <danfizesan@gmail.com>
2024-09-12 09:29:07 +0300

    align .bashrc content with code-server

--- setup_environment.sh
+++ setup_environment.sh
@@ -5,6 +5,9 @@
 #  wget -q --no-cache -O setup_environment.sh https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/setup_environment.sh && chmod +x setup_environment.sh && bash setup_environment.sh
 #
 ##################################
+rm .common_bashrc
+wget -q --no-cache -O .bashrc https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.common_bashrc
+
 rm .bashrc
 wget -q --no-cache -O .bashrc https://raw.githubusercontent.com/ObiDanKenobi/linux-configuration/main/.bashrc
 source .bashrc
